bae592f66846f71def27ddfb8801bf593173f607
gulegori <gulegori2019@gmail.com>
2026-08-20 01:13:42 +0000

    Criação do Projeto

---
+++ ecommerce/src/main/java/br/edu/unifio/ecommerce/entidades/Categoria.java
@@ -0,0 +1,13 @@
+package br.edu.unifio.ecommerce.entidades;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Categoria {
+    private Short id;
+    private String nome;
+    private String descricao;
+
+}
